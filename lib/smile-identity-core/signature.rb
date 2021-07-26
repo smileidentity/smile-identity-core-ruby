34449fd388152ff26486e12d374e46d5cc48aa76
Dan Bernier <danbernier@gmail.com>
2021-07-26 16:24:42 -0400

    Signature: group sec_key and signature methods together

--- lib/smile-identity-core/signature.rb
+++ lib/smile-identity-core/signature.rb
@@ -23,6 +23,20 @@ module SmileIdentityCore
       end
     end
 
+    def confirm_sec_key(timestamp, sec_key)
+      begin
+        hash_signature = Digest::SHA256.hexdigest([@partner_id.to_i, timestamp].join(":"))
+        encrypted = sec_key.split('|')[0]
+
+        public_key = OpenSSL::PKey::RSA.new(Base64.decode64(@api_key))
+        decrypted = public_key.public_decrypt(Base64.decode64(encrypted))
+
+        return decrypted == hash_signature
+      rescue => e
+        raise e
+      end
+    end
+
     def generate_signature(timestamp=Time.now.to_s)
       hmac = OpenSSL::HMAC.new(@api_key, 'sha256')
       hmac.update(timestamp.to_s)
@@ -42,20 +56,6 @@ module SmileIdentityCore
       hmac.update("sid_request")
       signature = Base64.strict_encode64(hmac.digest())
       return signature == msg_signature
-    end
-
-    def confirm_sec_key(timestamp, sec_key)
-      begin
-        hash_signature = Digest::SHA256.hexdigest([@partner_id.to_i, timestamp].join(":"))
-        encrypted = sec_key.split('|')[0]
-
-        public_key = OpenSSL::PKey::RSA.new(Base64.decode64(@api_key))
-        decrypted = public_key.public_decrypt(Base64.decode64(encrypted))
-
-        return decrypted == hash_signature
-      rescue => e
-        raise e
-      end
     end
   end
 end
