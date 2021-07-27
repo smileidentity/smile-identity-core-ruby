f54ce390d236fb9a21eb9984df0150ffa247acec
Dan Bernier <danbernier@gmail.com>
2021-07-27 16:26:50 -0400

    Utilities spec: ensure we raise when sec_key is invalid

--- spec/smile-identity-core/utilities_spec.rb
+++ spec/smile-identity-core/utilities_spec.rb
@@ -20,36 +20,51 @@ RSpec.describe SmileIdentityCore::Utilities do
   end
 
   describe '#query_job_status' do
-    let (:url) { 'https://some_server.com/dev01' }
-    let (:rsa) { OpenSSL::PKey::RSA.new(1024) }
-    let (:partner_id) { 1 }
-    let (:api_key) { Base64.encode64(rsa.public_key.to_pem) }
-    let (:timestamp)   { Time.now.to_i }
+    let(:url) { 'https://some_server.com/dev01' }
+    let(:rsa) { OpenSSL::PKey::RSA.new(1024) }
+    let(:partner_id) { 1 }
+    let(:api_key) { Base64.encode64(rsa.public_key.to_pem) }
+    let(:timestamp) { Time.now.to_i }
+    let(:good_sec_key) do
+      hash_signature = Digest::SHA256.hexdigest([partner_id, timestamp].join(":"))
+      [Base64.encode64(rsa.private_encrypt(hash_signature)), hash_signature].join('|')
+    end
 
     before(:each) {
       connection.instance_variable_set('@url', url )
       connection.instance_variable_set('@api_key', api_key)
       connection.instance_variable_set('@partner_id', partner_id)
-
-      hash_signature = Digest::SHA256.hexdigest([partner_id, timestamp].join(":"))
-      @sec_key = [Base64.encode64(rsa.private_encrypt(hash_signature)), hash_signature].join('|')
     }
 
-    it 'returns the response' do
-      body = {
-        timestamp: "#{timestamp}",
-        signature: "#{@sec_key}",
-        job_complete: true,
-        job_success: false,
-        code: "2302"
+    def a_signed_response(signature:, timestamp:)
+      {
+        timestamp: timestamp, signature: signature, job_complete: true, job_success: false, code: "2302"
       }.to_json
-
-      typhoeus_response = Typhoeus::Response.new(code: 200, body: body.to_s)
-      Typhoeus.stub(@url).and_return(typhoeus_response)
-
-      expect(connection.send(:query_job_status, '1', '1', {return_job_status: true, return_image_links: true})).to eq(JSON.load(body.to_s))
     end
 
+    it 'returns the response' do
+      response_body = a_signed_response(signature: good_sec_key, timestamp: timestamp)
+      typhoeus_response = Typhoeus::Response.new(code: 200, body: response_body)
+      Typhoeus.stub(@url).and_return(typhoeus_response)
+
+      expect(connection.send(:query_job_status, '1', '1', {return_job_status: true, return_image_links: true}))
+        .to eq(JSON.load(response_body))
+    end
+
+    context 'when the signature is invalid' do
+      it 'raises' do
+        response_body = a_signed_response(signature: "fake signature", timestamp: timestamp)
+        typhoeus_response = Typhoeus::Response.new(code: 200, body: response_body)
+        Typhoeus.stub(@url).and_return(typhoeus_response)
+
+        expect {
+          connection.send(:query_job_status, '1', '1', {return_job_status: true, return_image_links: true})
+        }.to raise_error(OpenSSL::PKey::RSAError)
+        # The specific error here matters less than the fact that it will raise.
+        # The code tries to raise a custom error, but we don't currently handle it properly.
+        # Leaving it this way for now, in case clients have come to rely on this behavior.
+      end
+    end
   end
 
   describe '#configure_job_query' do
