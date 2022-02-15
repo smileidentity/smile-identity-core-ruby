437e60c61d64e816042bcc6e31404f6e8b863dc5
sadick254 <sadickjunior@gmail.com>
2022-02-15 17:09:44 +0300

    Require files using glob pattern
    In case we add a class on or library, it will be automatically required.

--- lib/smile-identity-core.rb
+++ lib/smile-identity-core.rb
@@ -1,9 +1,4 @@
-require "smile-identity-core/version"
-require "smile-identity-core/web_api.rb"
-require "smile-identity-core/id_api.rb"
-require "smile-identity-core/signature.rb"
-require "smile-identity-core/utilities.rb"
-require "smile-identity-core/configuration.rb"
+Dir["#{File.dirname(__FILE__)}/smile-identity-core/**/*.rb"].sort.each { |f| require(f) }
 
 module SmileIdentityCore
   class Error < StandardError; end
