76b19bc1c1e42d3b2ded1aceba4f060965202a9d
Michael <michael.l.dangelo@gmail.com>
2022-10-25 05:35:45 -0700

    Format codebase with rubocop (#35)
    * format using rubocop
    
    * add rubocop to gemspec
    
    * add rubocop to ci action
    
    * add rubocop extensions
    
    * configure extensions
    
    * add || true
    
    * update changelog
    
    * downgrade bundler
    
    * drop support for ruby 2.5

--- spec/smile-identity-core/id_api_spec.rb
+++ spec/smile-identity-core/id_api_spec.rb
@@ -1,18 +1,20 @@
-RSpec.describe SmileIdentityCore::IDApi do
-  let (:partner_id) {'001'}
-  let (:api_key) {Base64.encode64( OpenSSL::PKey::RSA.new(1024).public_key.to_pem)}
-  let (:sid_server) {0}
-  let (:connection) { SmileIdentityCore::IDApi.new(partner_id, api_key, sid_server)}
+# frozen_string_literal: true
 
-  let (:partner_params) {
+RSpec.describe SmileIdentityCore::IDApi do
+  let(:partner_id) { '001' }
+  let(:api_key) { Base64.encode64(OpenSSL::PKey::RSA.new(1024).public_key.to_pem) }
+  let(:sid_server) { 0 }
+  let(:connection) { described_class.new(partner_id, api_key, sid_server) }
+
+  let(:partner_params) do
     {
       user_id: 'dmKaJazQCziLc6Tw9lwcgzLo',
       job_id: 'DeXyJOGtaACFFfbZ2kxjuICE',
       job_type: 5
     }
-  }
+  end
 
-  let (:id_info) {
+  let(:id_info) do
     {
       first_name: 'John',
       last_name: 'Doe',
@@ -23,32 +25,31 @@ RSpec.describe SmileIdentityCore::IDApi do
       phone_number: '0726789065',
       entered: 'true'
     }
-  }
+  end
 
   context 'ensure that the public methods behave correctly' do
-
     describe '#initialize' do
-      it "sets the partner_id, api_key, and sid_server instance variables" do
+      it 'sets the partner_id, api_key, and sid_server instance variables' do
         expect(connection.instance_variable_get(:@partner_id)).to eq(partner_id)
         expect(connection.instance_variable_get(:@api_key)).to eq(api_key)
       end
 
-      it "sets the correct @url instance variable" do
+      it 'sets the correct @url instance variable' do
         expect(connection.instance_variable_get(:@url)).to eq('https://testapi.smileidentity.com/v1')
 
-        connection = SmileIdentityCore::IDApi.new(partner_id, api_key, 'https://something34.api.us-west-2.amazonaws.com/something')
+        connection = described_class.new(partner_id, api_key, 'https://something34.api.us-west-2.amazonaws.com/something')
         expect(connection.instance_variable_get(:@url)).to eq('https://something34.api.us-west-2.amazonaws.com/something')
       end
     end
 
     describe '#submit_job' do
-      it "validates the partner_params" do
+      it 'validates the partner_params' do
         no_partner_parameters = nil
         array_partner_params = []
         missing_partner_params = {
           user_id: 'dmKaJazQCziLc6Tw9lwcgzLo',
           job_id: 'DeXyJOGtaACFFfbZ2kxjuICE',
-          job_type: nil,
+          job_type: nil
         }
 
         expect { connection.submit_job(no_partner_parameters, id_info) }
@@ -62,24 +63,25 @@ RSpec.describe SmileIdentityCore::IDApi do
       end
 
       it 'validates that a job type 5 was submitted' do
-        expect {
+        expect do
           connection.submit_job(
             { user_id: 'dmKaJazQCziLc6Tw9lwcgzLo', job_id: 'DeXyJOGtaACFFfbZ2kxjuICE', job_type: 1 },
-            id_info)
-        }.to raise_error(ArgumentError, 'Please ensure that you are setting your job_type to 5 to query ID Api')
+            id_info
+          )
+        end.to raise_error(ArgumentError, 'Please ensure that you are setting your job_type to 5 to query ID Api')
       end
 
       it 'validates the id_info' do
         expect { connection.submit_job(partner_params, nil) }
-          .to raise_error(ArgumentError, "Please make sure that id_info not empty or nil")
+          .to raise_error(ArgumentError, 'Please make sure that id_info not empty or nil')
         expect { connection.submit_job(partner_params, {}) }
-          .to raise_error(ArgumentError, "Please make sure that id_info not empty or nil")
+          .to raise_error(ArgumentError, 'Please make sure that id_info not empty or nil')
 
-        [:country, :id_type, :id_number].each do |key|
+        %i[country id_type id_number].each do |key|
           amended_id_info = id_info.merge(key => '')
 
           expect { connection.submit_job(partner_params, amended_id_info) }
-            .to raise_error(ArgumentError, "Please make sure that #{key.to_s} is included in the id_info")
+            .to raise_error(ArgumentError, "Please make sure that #{key} is included in the id_info")
         end
       end
     end
@@ -87,39 +89,39 @@ RSpec.describe SmileIdentityCore::IDApi do
 
   context 'ensure that the private methods behave correctly' do
     describe '#setup_requests' do
-      let(:url) {'https://www.example.com'}
+      let(:url) { 'https://www.example.com' }
 
-      before(:each) {
+      before do
         connection.instance_variable_set('@id_info', id_info)
         connection.instance_variable_set('@url', url)
-      }
+      end
 
-      it 'should return a correct json object if it runs successfully' do
+      it 'returns a correct json object if it runs successfully' do
         body = {
-          "JSONVersion": "1.0.0",
-          "SmileJobID": "0000001096",
+          "JSONVersion": '1.0.0',
+          "SmileJobID": '0000001096',
           "PartnerParams": {
-              "user_id": "dmKaJazQCziLc6Tw9lwcgzLo",
-              "job_id": "DeXyJOGtaACFFfbZ2kxjuICE",
-              "job_type": 5
+            "user_id": 'dmKaJazQCziLc6Tw9lwcgzLo',
+            "job_id": 'DeXyJOGtaACFFfbZ2kxjuICE',
+            "job_type": 5
           },
-          "ResultType": "ID Verification",
-          "ResultText": "ID Number Validated",
-          "ResultCode": "1012",
-          "IsFinalResult": "true",
+          "ResultType": 'ID Verification',
+          "ResultText": 'ID Number Validated',
+          "ResultCode": '1012',
+          "IsFinalResult": 'true',
           "Actions": {
-              "Verify_ID_Number": "Verified",
-              "Return_Personal_Info": "Returned"
+            "Verify_ID_Number": 'Verified',
+            "Return_Personal_Info": 'Returned'
           },
-          "Country": "NG",
-          "IDType": "BVN",
-          "IDNumber": "00000000000",
-          "ExpirationDate": "NaN-NaN-NaN",
-          "FullName": "some  person",
-          "DOB": "NaN-NaN-NaN",
-          "Photo": "Not Available",
-          "sec_key": "RKYX2ZVpvNTFW8oXdG2rerewererfCdFdRvika0bhJ13ntunAae85e1Fbw9NZli8PE0P0N2cbX5wNCV4Yag4PTCQrLjHG1ZnBHG/Q/Y+sdsdsddsa/rMGyx/m0Jc6w5JrrRDzYsr2ihe5sJEs4Mp1N3iTvQcefV93VMo18LQ/Uco0=|7f0b0d5ebc3e5499c224f2db478e210d1860f01368ebc045c7bbe6969f1c08ba",
-          "timestamp": 1570612182124
+          "Country": 'NG',
+          "IDType": 'BVN',
+          "IDNumber": '00000000000',
+          "ExpirationDate": 'NaN-NaN-NaN',
+          "FullName": 'some  person',
+          "DOB": 'NaN-NaN-NaN',
+          "Photo": 'Not Available',
+          "sec_key": 'RKYX2ZVpvNTFW8oXdG2rerewererfCdFdRvika0bhJ13ntunAae85e1Fbw9NZli8PE0P0N2cbX5wNCV4Yag4PTCQrLjHG1ZnBHG/Q/Y+sdsdsddsa/rMGyx/m0Jc6w5JrrRDzYsr2ihe5sJEs4Mp1N3iTvQcefV93VMo18LQ/Uco0=|7f0b0d5ebc3e5499c224f2db478e210d1860f01368ebc045c7bbe6969f1c08ba',
+          "timestamp": 1_570_612_182_124
         }.to_json
 
         response = Typhoeus::Response.new(code: 200, body: body)
@@ -136,7 +138,8 @@ RSpec.describe SmileIdentityCore::IDApi do
 
         # this test does not directly relate to the implementation of the library but it will help us to debug
         # if any keys get removed from the response which will affect the partner.
-        expect(JSON.parse(setup_response).keys).to match_array(%w[JSONVersion SmileJobID PartnerParams ResultType ResultText ResultCode IsFinalResult Actions Country IDType IDNumber ExpirationDate FullName DOB Photo sec_key timestamp])
+        expect(JSON.parse(setup_response).keys).to match_array(%w[JSONVersion SmileJobID PartnerParams ResultType
+                                                                  ResultText ResultCode IsFinalResult Actions Country IDType IDNumber ExpirationDate FullName DOB Photo sec_key timestamp])
       end
     end
 
@@ -147,7 +150,7 @@ RSpec.describe SmileIdentityCore::IDApi do
         connection.instance_variable_set(:@partner_params, 'any partner params')
       end
 
-      it "returns a hash formatted for the request" do
+      it 'returns a hash formatted for the request' do
         parsed_response = JSON.parse(connection.send(:configure_json))
         expect(parsed_response).to match(
           'timestamp' => /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [-+]\d{4}/, # new signature!,
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler/setup'
 require 'simplecov'
 
@@ -9,7 +11,7 @@ require 'smile-identity-core'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -18,7 +20,7 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.before :each do
+  config.before do
     Typhoeus::Expectation.clear
   end
 end
