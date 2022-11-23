3514eee822a3a8dc766f8309bee8fcb8f30dce45
Barnabas Nsoh <banasco@gmail.com>
2022-11-23 09:25:34 +0000

    Add business verification to readme Update business verification example to use Webapi

--- examples/business_verification.rb
+++ examples/business_verification.rb
@@ -11,7 +11,7 @@ default_callback = '<Put your default callback url here>'
 api_key = '<Put your API key here>'; # copy your API key from the Smile Identity portal
 sid_server = '<0 | 1>'; # Use '0' for the sandbox server, use '1' for production server
 
-connection = SmileIdentityCore::BusinessVerification.new(partner_id, '', api_key, sid_server)
+connection = SmileIdentityCore::WebApi.new(partner_id, default_callback, api_key, sid_server)
 
 # Create required tracking parameters
 partner_params = {
@@ -24,9 +24,9 @@ partner_params = {
 id_info = {
   country: '<2-letter country code>', # The country where ID document was issued
   id_type: '<id type>', # The ID document type
-  id_number: '0000000', # The business registration or tax number
+  id_number: '<id number>', # The business registration or tax number
   business_type: 'co'
 }
 
 # Submit the job
-response = connection.submit_job(partner_params, id_info)
+connection.submit_job(partner_params, [], id_info, {})
